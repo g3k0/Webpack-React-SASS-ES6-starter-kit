74429002d7123844303ec24a56a56d0af0fe68dc
Christian Palazzo <info@christianpalazzo.org>
2016-09-22 12:41:04 +0200

    webpack image loading and dependencies installation

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
 				}
 			},
 			{
+				test: /\.(png|jpg)$/,
+				loader: 'url-loader'
+			},
+			{
 				test: /\.sass$/,
 				loader: 'style-loader!css-loader!sass-loader'
 			}
