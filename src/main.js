fe801c05bbd893f83692311ca8ab41e1cf4319ad
Christian Palazzo <info@christianpalazzo.org>
2016-09-22 11:53:05 +0200

    css loaders, react component and dependencies installation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
-const $ = require('jquery');
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./style.css');
 
-$('#target').html('hello world');
+class Message extends React.Component {
+	constructor(props) {
+	    super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>{this.props.title}</h1>
+				<p>{this.props.message}</p>
+			</div>);
+	}
+}
+
+ReactDOM.render(<Message title="Email Joe" message="can you email him?" />,
+	document.getElementById('react-container')
+);
